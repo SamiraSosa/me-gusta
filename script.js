9b3ea8b5f6c5ba5869567813659da3825b405fba
Samira Sosa <sosasamira844@gmail.com>
2023-11-23 23:49:32 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var cont=9;
+var sumar = document.querySelector("#mas9");
+function aumento(){
+    cont++;
+    sumar.innerText= cont + " like(s)";
+}
+
+var cont2=9;
+var sumar2 = document.querySelector("#nueve");
+function aumentoII(){
+    cont2++;
+    sumar2.innerText= cont2 + " like(s)";
+}
+
+var cont3=12;
+var sumar3 = document.querySelector("#aumentar12");
+function aumentoIII(){
+    cont3++;
+    sumar3.innerText= cont3 + " like(s)";
+}
